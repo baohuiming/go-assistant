f0fb8ce805f7e4d91b482da89586c4f77b2fa535
Bao Huiming <m@baohuiming.net>
2024-11-11 14:26:50 +0800

    feat: update cross-build.bat

--- cross_build.bat
+++ cross_build.bat
@@ -1,22 +1,25 @@
+SET TARGET=dist
+SET EXECUTE_NAME=go_assistant
+
 SET CGO_ENABLED=0
 
 SET GOOS=linux
 SET GOARCH=amd64
-go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o dist/go_assistant_linux_amd64
+go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o %TARGET%/%EXECUTE_NAME%_linux_amd64
 
 SET GOARCH=386
-go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o dist/go_assistant_linux_386
+go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o %TARGET%/%EXECUTE_NAME%_linux_386
 
 SET GOARCH=arm64
-go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o dist/go_assistant_linux_aarch64
+go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o %TARGET%/%EXECUTE_NAME%_linux_aarch64
 
 SET GOARCH=arm
 SET GOARM=7
-go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o dist/go_assistant_linux_armv7
+go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o %TARGET%/%EXECUTE_NAME%_linux_armv7
 
 SET GOOS=windows
 SET GOARCH=amd64
-go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o dist/go_assistant_windows_x64.exe
+go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o %TARGET%/%EXECUTE_NAME%_windows_x64.exe
 
 SET GOARCH=386
-go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o dist/go_assistant_windows_x86.exe
+go build -v -a -ldflags "-s -w" -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}" -o %TARGET%/%EXECUTE_NAME%_windows_x86.exe
